db123f1d1a8d1ba13a4166e80ed6697ca0768726
Andy Uzal <andyuzal@gmail.com>
2026-02-03 20:20:22 +0100

    feat: Graphics template

---
+++ src/utils/graphics.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mplsoccer import Pitch
+
+
+class Graphics:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+# Visualizar los tiros de ambos equipos con XG y nombre de jugador
+def plot_event_shots(matches, shots):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    pitch = Pitch(pitch_color="white", line_color="black")
+    pitch.draw(ax=ax)
+    for i in range(len(shots)):
+        if shots.team_name.values[i] == matches.away_team.values[0]:
+            if shots.shot_outcome_name.values[i] == "Goal":
+                pitch.scatter(
+                    shots.location_x.values[i],
+                    shots.location_y.values[i],
+                    ax=ax,
+                    color="red",
+                    s=shots.shot_statsbomb_xg.values[i] * 1000,
+                )
+                plt.text(
+                    shots.location_x.values[i],
+                    shots.location_y.values[i],
+                    shots.player_name.values[i],
+                )
+            else:
+                pitch.scatter(
+                    shots.location_x.values[i],
+                    shots.location_y.values[i],
+                    ax=ax,
+                    color="red",
+                    s=shots.shot_statsbomb_xg.values[i] * 1000,
+                    alpha=0.3,
+                )
+        else:
+            if shots.shot_outcome_name.values[i] == "Goal":
+                pitch.scatter(
+                    shots.location_x.values[i],
+                    80 - shots.location_y.values[i],
+                    ax=ax,
+                    color="blue",
+                    s=shots.shot_statsbomb_xg.values[i] * 1000,
+                )
+                plt.text(
+                    shots.location_x.values[i],
+                    80 - shots.location_y.values[i],
+                    shots.player_name.values[i],
+                )
+            else:
+                pitch.scatter(
+                    shots.location_x.values[i],
+                    80 - shots.location_y.values[i],
+                    ax=ax,
+                    color="blue",
+                    s=shots.shot_statsbomb_xg.values[i] * 1000,
+                    alpha=0.3,
+                )
+    plt.text(15, 5, matches.away_team.values[0] + " shots")
+    plt.text(80, 5, matches.home_team.values[0] + " shots")
+    plt.title(
+        "{} {} - {} Vs {}".format(
+            matches.competition.values[0],
+            matches.match_date.values[0],
+            matches.home_team.values[0],
+            matches.away_team.values[0],
+        ),
+        fontsize=16,
+    )
+    plt.show()
+
+# Ejemplo de uso: plot_event_shots(matches, shots)
+
+# Visualizar el eventing pase (Exitosos y fallidos)
+def draw_pass_event(df, team):
+    df = df[df.team_name == team]
+    mask = df.pass_outcome_name.isnull()
+
+    fig, ax = plt.subplots(figsize=(15,10))
+    pitch = Pitch(pitch_color='#22312b', line_color='white')
+    pitch.draw(ax=ax)
+
+    pitch.arrows(df[mask].location_x, df[mask].location_y, 
+               df[mask].pass_end_location_x, df[mask].pass_end_location_y,
+               color = '#ad993c', ax=ax,width=2, headwidth=10,
+               headlength=10, label='Complete passes')
+    pitch.arrows(df[~mask].location_x, df[~mask].location_y,
+                 df[~mask].pass_end_location_x, df[~mask].pass_end_location_y,
+                 color ='#ba4f45', ax=ax,width=2, headwidth=10,
+                 headlength=10, label='Failed passes')
+    ax.legend(handlelength=5, edgecolor='white', fontsize=10, loc='lower left')
+    plt.title('Passes {}'.format(team), fontsize=16)
+    plt.show()
+
+# Ejemplo de uso: draw_pass_event(pases, 'Tottenham Hotspur')
+
+
+# Visualizar mapa de calor ( puede usarse para los pases o coordenadas de recorrido de los jugadores)
+def plot_heat_map_pass(df, team, x, y, title):
+    df = df[df.team_name == team]
+    fig, ax = plt.subplots(figsize=(15,10))
+    pitch = Pitch(pitch_color='#22312b', line_color='white')
+    pitch.draw(ax=ax)
+
+    kde = sns.kdeplot(x=df[x], y=df[y], shade=True, shade_lowest=False,
+                      thresh=0.4, alpha=.4, n_levels=10, cmap='CMRmap')
+    plt.title(title, fontsize=16)
+
+    plt.show()
+
+# Ejemplo de uso: plot_heat_map_pass(pases, 'Tottenham Hotspur', 'pass_end_location_x', 'pass_end_location_y', 'Mapa de calor de los pases realizados por el Tottenham')
+
+
+# Visualizar la matriz de pases por equipo
+def plot_pass_matrix(df, team):
+ df = df[df.team_name == team]
+ df_pas = df[df.pass_outcome_name.isnull()]
+
+ avg = df_pas.groupby('player_name').agg({'location_x':'mean', 'location_y':['mean', 'count']})
+
+ avg.columns = ['x_avg', 'y_avg', 'count']
+
+ btw = df_pas.groupby(['player_name', 'pass_recipient_name']).id.count().reset_index()
+
+ merg1 = btw.merge(avg, left_on='player_name', right_index=True)
+ pass_btw = merg1.merge(avg, left_on='pass_recipient_name', right_index=True, suffixes=['', '_end'])
+
+ pas_rec = pass_btw.groupby('pass_recipient_name').id.sum()
+ new_avg = pd.concat([avg, pas_rec],axis=1)
+ 
+ fig, ax = plt.subplots(figsize=(15,10))
+ pitch = Pitch(pitch_color='#22312b', line_color='white')
+ pitch.draw(ax=ax)
+
+ for i in range(len(pass_btw)):
+    arrows = pitch.arrows(pass_btw.x_avg.values[i], pass_btw.y_avg.values[i], pass_btw.x_avg_end.values[i], pass_btw.y_avg_end.values[i], ax=ax, 
+                         color='white', width=pass_btw.id.values[i], headwidth=2,headlength=2,
+                         headaxislength=2, zorder=1, alpha=0.5)
+    nodes = pitch.scatter(pass_btw.x_avg.values[i], pass_btw.y_avg.values[i],
+                          s=pass_btw['count'].values[i]*100, color='red', ax=ax, alpha=0.5)
+for i in range(len(avg)):
+ pitch.annotate((new_avg['id'].values[i] ,new_avg['count'].values[i]),
+ xy=(new_avg['x_avg'].values[i], new_avg['y_avg'].values[i]), ax=ax,
+ va='center', ha='center',color = 'white')
+
+# Ejemplo de uso: plot_pass_matrix(pases, 'Tottenham Hotspur')
